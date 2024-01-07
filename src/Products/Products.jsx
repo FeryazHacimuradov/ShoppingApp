3c47b66b3b8ac294e4b4f58d803a39fca63e59c2
“FeryazHacimuradov” <tu201906125@code.edu.az>
2024-01-07 14:18:55 +0400

    Create product and gived style

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,5 +1,39 @@
 import "./products.scss";
+import { AiFillStar } from "react-icons/ai";
+import { BsFillBagFill } from "react-icons/bs";
 
 export default function Products() {
-  return <div>Products</div>;
+  return (
+    <>
+      <section className="card-container">
+        <section className="card">
+          <img
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="Shoe"
+            className="card-img"
+          />
+          <div className="card-details">
+            <h3 className="card-title">Shoe</h3>
+            <section className="card-reviews">
+              <AiFillStar className="ratings-start" />
+              <AiFillStar className="ratings-start" />
+              <AiFillStar className="ratings-start" />
+              <AiFillStar />
+              <AiFillStar />
+              <span className="total-reviews">5</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>300$</del> 200$
+              </div>
+
+              <div className="bag">
+                <BsFillBagFill className="bag-icon" />
+              </div>
+            </section>
+          </div>
+        </section>
+      </section>
+    </>
+  );
 }
