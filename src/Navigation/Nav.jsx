0c51907fe8f041d9fe35a4136a17b99c43d2eeae
“FeryazHacimuradov” <tu201906125@code.edu.az>
2024-01-07 13:57:59 +0400

    Created simple navbar

--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -1,5 +1,30 @@
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+
 import "./nav.scss";
 
 export default function Nav() {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <div className="nav-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Enter your search shoes."
+        />
+      </div>
+
+      <div className="profile-container">
+        <a href="#">
+          <FiHeart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineShoppingCart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineUserAdd className="nav-icons" />
+        </a>
+      </div>
+    </nav>
+  );
 }
